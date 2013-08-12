c5672a184e0a89337058c5471f88cf8a53a775b4
OpenShift guest <zhaol@hawaii.edu>
2013-08-12 03:35:32 -0400

    format pointer.c output

--- code/03a_pointer/pointer.c
+++ code/03a_pointer/pointer.c
@@ -5,24 +5,24 @@ main() {
     int* integer_pointer = &integer;
 
     printf ("===integer===\n");
-    printf ("integer (value of 'integer' variable): %d\n", integer);
-    printf ("&integer (address of 'integer' variable): %p\n", &integer);
-    printf ("*integer is a syntax error\n");
-    printf ("*(&integer) (value of (address of 'integer' variable)): %d\n", *(&integer));
+    printf ("%20d: integer, value of 'integer' variable\n", integer);
+    printf ("%20p: &integer, address of 'integer' variable\n", &integer);
+    printf ("%20s: *integer is a syntax error\n", "");
+    printf ("%20d: *(&integer), value of (address of 'integer' variable)\n", *(&integer));
     printf ("===integer_pointer===\n");
-    printf ("integer_pointer (value of 'integer_pointer' variable): %p\n", integer_pointer);
-    printf ("*integer_pointer (value of the object that the 'integer_pointer' variable points to): %d\n", *integer_pointer);
-    printf ("&integer_pointer (address of 'integer_pointer' variable): %p\n", &integer_pointer);
+    printf ("%20p: integer_pointer, value of 'integer_pointer' variable\n", integer_pointer);
+    printf ("%20d: *integer_pointer, value of the object that the 'integer_pointer' variable points to\n", *integer_pointer);
+    printf ("%20p: &integer_pointer, address of 'integer_pointer' variable\n", &integer_pointer);
     
     float real_number = 1.1;
     float* real_number_pointer = &real_number;  
     printf ("===real_number===\n");
-    printf ("real_number (value of 'real_number' variable): %f\n", real_number);
-    printf ("&real_number (address of 'real_number' variable): %p\n", &real_number);
-    printf ("*real_number is a syntax error\n");
-    printf ("*(&real_number) (value of (address of 'real_number' variable)): %f\n", *(&real_number));
+    printf ("%20f: real_number, value of 'real_number' variable\n", real_number);
+    printf ("%20p: &real_number, address of 'real_number' variable\n", &real_number);
+    printf ("%20s: *real_number is a syntax error\n", "");
+    printf ("%20f: *(&real_number), value of (address of 'real_number' variable)\n", *(&real_number));
     printf ("===real_number_pointer===\n");
-    printf ("real_number_pointer (value of 'real_number_pointer' variable): %p\n", real_number_pointer);
-    printf ("*real_number_pointer (value of the object that the 'real_number_pointer' variable points to): %f\n", *real_number_pointer);
-    printf ("&real_number_pointer (address of 'real_number_pointer' variable): %p\n", &real_number_pointer);
+    printf ("%20p: real_number_pointer, value of 'real_number_pointer' variable\n", real_number_pointer);
+    printf ("%20f: *real_number_pointer, value of the object that the 'real_number_pointer' variable points to\n", *real_number_pointer);
+    printf ("%20p: &real_number_pointer, address of 'real_number_pointer' variable\n", &real_number_pointer);
 }
