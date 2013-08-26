5b14ba4b2f53c28933c787bdf2f91c7b7257b195
OpenShift guest <zhaol@hawaii.edu>
2013-08-26 00:41:38 -0400

    update assignment code

--- code/assignments/02_print_format/variables.c
+++ code/assignments/02_print_format/variables.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
 
 main() {
-    int i_am_an_integer;
-    char i_am_a_char;
-    float i_am_a_float;
-    double i_am_a_double;
-
-    i_am_an_integer = 11;
-    i_am_a_char = 'c';
-    i_am_a_float = 1.23;
-    i_am_a_double = 1.23;
-
-    printf("This is an integer: %i \n", i_am_an_integer);
-    printf("This is an char: %c \n", i_am_a_char);
-    printf("This is an float: %f \n", i_am_a_float);
-    printf("This is an double: %f \n", i_am_a_double);
+    double sample_float = 1.2345000;
+    
+    printf("zero padding in back of a number: %.5f %.6f %.7f\n", sample_float, sample_float, sample_float);
+    printf("zero padding in front of a number: %05.4f %07.4f %08.4f\n", sample_float, sample_float, sample_float);
+    printf("exponentials (base 10): %.4e %.5e %.6e\n", sample_float, sample_float, sample_float);
+    printf("truncation vs rounding of numbers: %.3f \n", sample_float); //note the answer is not 1.235
+    printf("negative numbers: %.4f %.4e\n", -1*sample_float, -1*sample_float);
 }
