424d4971c23e4a0aaebba26d0fc50f2e63d129a4
OpenShift guest <zhaol@hawaii.edu>
2013-08-26 00:54:01 -0400

    update bye_world.c to exercise character escaping

--- code/assignments/01_bye_world/bye_world.c
+++ code/assignments/01_bye_world/bye_world.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 main() {
-    printf("It was nice meeting you, world.\n");
+    printf("It was nice meeting you, world. :-\\\n");
     printf("Hope to see you again soon!\n");
-    printf("Don't forget to write. :)\n");
+    printf("Don't forget to write.\n");
 }
