a957d45c590c4a7a75ec25bc3ad15c5a0e0bc6d7
OpenShift guest <zhaol@hawaii.edu>
2013-08-25 22:55:59 -0400

    fix bye_world.c

--- code/assignments/01_bye_world/bye_world.c
+++ code/assignments/01_bye_world/bye_world.c
@@ -2,6 +2,6 @@
 
 main() {
     printf("It was nice meeting you, world.\n");
-    printf("Hope to see you again soon.\n");
+    printf("Hope to see you again soon!\n");
     printf("Don't forget to write. :)\n");
 }
