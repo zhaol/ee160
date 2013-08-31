c56b91b174da6e3ee24a126f858c32913497f0b8
Zhao Li <zhaol@hawaii.edu>
2013-08-31 12:46:00 -0400

    update fix_me.c to remove dependency on scanf

--- code/03_fix_me/fix_me.c
+++ code/03_fix_me/fix_me.c
@@ -7,17 +7,13 @@ main() {
     int second_number;
     int result
 
-    printf ("Enter the first number: ");
-    scanf ("%f", &first_number);
+    first_number = 3;
+    second_number = 5;
     
-    prinf ("Enter the second number: ");
-    scanf ("%d", second_number);
-    
+    first_number = first_number + 1;
     #ifdef DEBUG
         printf("[DEBUG]: The intermediary result is: %d \n", (first_number + second_number));
     #endif
-    
-    first_number = first_number + 1;
     result = (first_number + second_number) * second_number;
     
     printf ("The result is: %d \n", result);
