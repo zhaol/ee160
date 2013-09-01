83f7f17e61dbc13dd28ec86eb89e1ffed5a850e3
Zhao Li <zhaol@hawaii.edu>
2013-08-31 20:15:22 -0400

    add fix_me_too.c

---
+++ code/fix_me_too/fix_me_too.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+main{} (
+    int first_number;
+    float second_number;
+    char special_letter;
+
+    first_number = 3.0;
+    second_number = 5;
+    special_letter = '/';
+    
+    printf ('////');
+    printf ("//The first number is %f//", first_number)
+    printf {"//The second number is %d//", second_number};
+    printf (//The special character is \"%c\"//", special_leter);
+)
