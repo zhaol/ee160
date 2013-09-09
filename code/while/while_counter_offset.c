93975b4584504edb9b00c946a65fc67a01e66874
Zhao Li <zhaol@hawaii.edu>
2013-09-09 03:09:44 -0400

    update while example code and wording to be clearer

--- code/while/while_counter_offset.c
+++ code/while/while_counter_offset.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 main() {
-  int number_of_iterations = 3;
   int counter;
 
   counter = 0;
-  while (counter < number_of_iterations) {
+  while (counter < 3) {
     printf ("Hip, hip, hooray!\n");
-    if (counter == number_of_iterations - 1) {
+    if (counter == 2) {
       printf ("That was the last cheer. :(\n");
     }
     counter = counter + 1;
