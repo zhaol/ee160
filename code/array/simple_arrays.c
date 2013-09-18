3452e07eba8723fdc906b0aedcfc38af3e80f92c
Zhao Li <zhaol@hawaii.edu>
2013-09-18 00:27:57 -0400

    update array lecture and example code

--- code/array/simple_arrays.c
+++ code/array/simple_arrays.c
@@ -15,20 +15,4 @@ main() {
   printf("list_of_values[0]: %d\n", list_of_values[0]);
   printf("list_of_values[1]: %d\n", list_of_values[1]);
   printf("list_of_values[2]: %d\n", list_of_values[2]);
-  
-  char string[] = "word ";
-  
-  printf("string[0]: %c\n", string[0]);
-  printf("string[1]: %c\n", string[1]);
-  printf("string[2]: %c\n", string[2]);
-  printf("string[3]: %c\n", string[3]);
-  printf("string[4]: @%c@\n", string[4]);
-  printf("string[5]: @%c@\n", string[5]);
-  printf("string[6]: @%c@\n", string[6]);
-  printf("null character \0: @%c@\n", '\0');
-  
-  printf("(int)string[4]: @%i@\n", string[4]);
-  printf("(int)string[5]: @%i@\n", string[5]);
-  printf("(int)string[6]: @%i@\n", string[6]);
-  printf("(int)null character \0: @%i@\n", '\0');
 }
