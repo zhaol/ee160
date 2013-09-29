534ce53a3cd5007e7aa527302b9129b7cb696289
Zhao Li <zhaol@hawaii.edu>
2013-09-29 14:22:03 -0400

    update and add example code

---
+++ code/functions/add.c
@@ -0,0 +1,21 @@
+/*   File:  add.c
+//   By:    The Awesome Class of EE160
+//   Date:	Today :)
+*/
+
+#include <stdio.h>
+
+// This program uses a function to add 2 numbers
+int main() {
+  int number1;
+  int number2;
+  
+  printf("Please enter in the first number:\n");
+  scanf("%d", &number1);
+  printf("Please enter in the second number:\n");
+  scanf("%d", &number2);
+
+  printf ("The sum of your two numbers is: %d\n", add(number1, number2));
+  
+  return 0;
+}
