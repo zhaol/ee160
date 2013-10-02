9d353f4b03f109affa24b0ac91c1e096004cb7ac
Zhao Li <zhaol@hawaii.edu>
2013-10-01 21:53:25 -0400

    wrap up functions lecture with example and assignment

---
+++ code/functions/add_now.c
@@ -0,0 +1,23 @@
+/*   File:  add_now.c
+//   By:    The Awesome Class of EE160
+//   Date:	Today :)
+*/
+
+#include <stdio.h>
+
+// This program uses a function to increment 2 numbers
+int main() {
+  int number1;
+  int number2;
+  
+  printf("Please enter in the first number:\n");
+  scanf("%d", &number1);
+  printf("Please enter in the second number:\n");
+  scanf("%d", &number2);
+
+  add_now(&number1, &number2);
+  printf("The first number is now: %d\n", first_number);
+  printf("The second number is now: %d\n", second_number);
+  
+  return 0;
+}
