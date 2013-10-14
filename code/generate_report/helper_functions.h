17d8ff572721120ada06bee0deaa0b5fc922cff2
Zhao Li <zhaol@hawaii.edu>
2013-10-14 14:12:30 -0400

    add missing function prototype to past example code

--- code/generate_report/helper_functions.h
+++ code/generate_report/helper_functions.h
@@ -6,3 +6,4 @@ int is_account_below_minimum_balance(float account_balance, float minimum_balanc
 int ask_to_exit();
 void update_account_summary_information(int* total_number_of_accounts, float* total_amount_in_accounts, float account_balance);
 void output_account_summary_to_file(int total_number_of_accounts, float total_amount_in_accounts, float total_minimum_balance_fees, int total_accounts_with_minimum_balance_fees);
+void output_list_of_account_balances(float account_balances[], int number_of_accounts);
