d2a517b7715e471b3c2aec8fff92f7268e610734
Zhao Li <zhaol@hawaii.edu>
2013-11-06 16:18:28 -0500

    touch up else if syntax

--- code/comparisons/else_if_vs_switch.c
+++ code/comparisons/else_if_vs_switch.c
@@ -10,11 +10,11 @@ int main () {
   int number = 201;
   
   // else if can match ranges
-  if ((number >= 0) && (number < 100)) {
+  if ((0 <= number) && (number < 100)) {
     printf("The number is between 0 and 100, including 0\n");  
-  } else if ((number >= 100) && (number < 200)) {
+  } else if ((100 <= number) && (number < 200)) {
     printf("The number is between 100 and 200, including 100\n");
-  } else if ((number >= 200) && (number < 300)) {
+  } else if ((200 <= number) && (number < 300)) {
     printf("The number is between 200 and 300, including 200\n");
   } else {
     printf("The number is not (0 - 100), (100 - 200), or (200 - 300)\n");
