29bc71556d8fac5ad27206c0e7f3de94d4f568ae
Zhao Li <zhaol@hawaii.edu>
2013-11-07 08:57:42 -0500

    fix up for and do while lecture; add assignment

---
+++ code/for/for_with_multiple.c
@@ -0,0 +1,17 @@
+/*   File:  for_with_multiple.c
+//   By:    The Awesome Class of EE160
+//   Date:	Today :)
+*/
+
+#include <stdio.h>
+
+// This program demonstrates using a for loop with multiple counters
+int main () {
+  int index1, index2;
+  
+  for (index1=0, index2=10; index1<=index2; index1++, index2--) {
+    printf("index1: %d, index2: %d\n", index1, index2);
+  }
+  
+  return 0;  
+}
