e441322d6f945454606a723400abffb5062c031e
Zhao Li <zhaol@hawaii.edu>
2013-11-11 14:30:25 -0500

    update wording in pointer arithmetic example code

--- code/pointer_arithmetic/display_2d_array.c
+++ code/pointer_arithmetic/display_2d_array.c
@@ -19,7 +19,7 @@ int main () {
     // printf("array[%d]: %d\n", pointer_counter + ARRAY_OFFSET, *(array_pointer + pointer_counter + ARRAY_OFFSET)); // this is not very intuitive but it gives you an idea of how these values are being stored by C
   }
   
-  /* You would need a nested loop if you are using array indexing
+  /* You would (sort of) need a nested loop if you are using array indexing
   int col_counter, row_counter;
   for (row_counter=1; row_counter<=MAX_ROW; row_counter++) {
     for (col_counter=1; col_counter<=MAX_COL; col_counter++) {
