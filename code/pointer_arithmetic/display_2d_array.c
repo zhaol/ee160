6c03c3df9afc65fbf9488a2c09aa685987e0327d
Zhao Li <zhaol@hawaii.edu>
2013-11-16 10:17:39 -0500

    add pointer arithmetic printout with functions

--- code/pointer_arithmetic/display_2d_array.c
+++ code/pointer_arithmetic/display_2d_array.c
@@ -13,20 +13,36 @@ int main () {
   int array[MAX_ROW][MAX_COL] = {{11, 12, 13}, {14, 15, 16}};
   int *array_pointer = &array[0][0];
   int pointer_counter;
+  int col_counter, row_counter;
   
+  // Block 1:
+  printf("Using pointers:\n");
   for (pointer_counter=1; pointer_counter<=MAX_COL*MAX_ROW; pointer_counter++) {
-    printf("array_pointer + %d: %d\n", pointer_counter + ARRAY_OFFSET, *(array_pointer + pointer_counter + ARRAY_OFFSET));  
-    // printf("array[%d]: %d\n", pointer_counter + ARRAY_OFFSET, *(array_pointer + pointer_counter + ARRAY_OFFSET)); // this is not very intuitive but it gives you an idea of how these values are being stored by C
+    printf("*(array_pointer + %d): %d\n", pointer_counter + ARRAY_OFFSET, *(array_pointer + pointer_counter + ARRAY_OFFSET));  
   }
   
-  /* You would (sort of) need a nested loop if you are using array indexing
-  int col_counter, row_counter;
+  // Block 2:
+  // This block of code shows the full equation used in block 1 and is described here: https://docs.google.com/drawings/d/1lMj5aNmwSmbp_TGJmPZXcOg7I2sgMXjub7_mgDyLRcA/pub?w=822&h=540
+  printf("\nUsing pointers and showing the full equation:\n");
+  printf("(MAX_COL is 3)\n");
+  for (row_counter=1; row_counter<=MAX_ROW; row_counter++) {
+    for (col_counter=1; col_counter<=MAX_COL; col_counter++) {
+      printf("*(array_pointer + (%d * %d) + %d): %d\n",
+        row_counter+ARRAY_OFFSET,
+        MAX_COL,
+        col_counter+ARRAY_OFFSET,
+        *(array_pointer + ((row_counter+ARRAY_OFFSET) * MAX_COL) + col_counter+ARRAY_OFFSET));
+    }
+  }
+  
+  // Block 3:
+  // You need a nested loop if you are using array indexing
+  printf("\nUsing array indexing:\n");
   for (row_counter=1; row_counter<=MAX_ROW; row_counter++) {
     for (col_counter=1; col_counter<=MAX_COL; col_counter++) {
       printf("array[%d][%d]: %d\n", row_counter+ARRAY_OFFSET, col_counter+ARRAY_OFFSET, array[row_counter+ARRAY_OFFSET][col_counter+ARRAY_OFFSET]);
     }
   }
-  */
   
   return 0;
 }
