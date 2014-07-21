644f8c9cf8f61a6022e73425f50ed85bc99dcd12
Zhao Li <zhaol@hawaii.edu>
2014-07-21 04:32:26 -0400

    update if else lecture; add another if demo

--- code/if/if.c
+++ code/if/if.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 main() {
-    int number;
+  int number;
 
-    printf ("Enter a magical number: ");
-    scanf ("%d", &number);
-    
-    if (number >= 100) {
-        printf ("Your number is very magical! \n");
-    } else {
-        printf ("Your number needs more magic :( \n");    
-    }
+  printf ("Enter a magical number: ");
+  scanf ("%d", &number);
+  
+  if (number >= 100) {
+    printf ("Your number is very magical! \n");
+  } else {
+    printf ("Your number needs more magic :( \n");    
+  }
 }
