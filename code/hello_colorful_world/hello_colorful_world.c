ba29396fc8428ddcd630332f3002c5f1aa2f4e3c
Zhao Li <zhaol@hawaii.edu>
2014-08-23 17:09:28 +0000

    update hello colorful world to be more readable

--- code/hello_colorful_world/hello_colorful_world.c
+++ code/hello_colorful_world/hello_colorful_world.c
@@ -1,14 +1,37 @@
 #include <stdio.h>
 
-#define KNRM  "\x1B[0m"
-#define KRED  "\x1B[31m"
-#define KGRN  "\x1B[32m"
-#define KYEL  "\x1B[33m"
-#define KBLU  "\x1B[34m"
-#define KMAG  "\x1B[35m"
-#define KCYN  "\x1B[36m"
-#define KWHT  "\x1B[37m"
+#define NORMAL  "\x1B[0m"
+#define BLACK   "\x1B[30m"
+#define RED     "\x1B[31m"
+#define GREEN   "\x1B[32m"
+#define YELLOW  "\x1B[33m"
+#define BLUE    "\x1B[34m"
+#define MAGENTA "\x1B[35m"
+#define CYAN    "\x1B[36m"
+#define WHITE   "\x1B[37m"
   
 main() {
-  printf("Hello %sc%so%sl%so%sr%sf%su%sl %sworld\n", KRED, KGRN, KYEL, KBLU, KMAG, KCYN, KRED, KBLU, KNRM);
+  printf(        "Hello ");
+  printf(RED     "c");
+  printf(GREEN   "o");
+  printf(YELLOW  "l");
+  printf(BLUE    "o");
+  printf(MAGENTA "r");
+  printf(CYAN    "f");
+  printf(WHITE   "u");
+  printf(BLACK   "l");
+  printf(NORMAL  " world\n");
+  // this is equivalent to:
+  /*
+  printf("Hello ");
+  printf("\x1B[31mc");
+  printf("\x1B[32mo");
+  printf("\x1B[33ml");
+  printf("\x1B[34mo");
+  printf("\x1B[35mr");
+  printf("\x1B[36mf");
+  printf("\x1B[37mu");
+  printf("\x1B[30ml");
+  printf("\x1B[0m world\n");
+  */
 }
