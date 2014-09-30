45be8dbf29a9dd7b9eaad1a66a3fc581a231eb6d
Zhao Li <zhaol@hawaii.edu>
2014-09-30 16:25:41 +0000

    add example operator code

---
+++ code/operators/modulus.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+  int dividend = 11;
+  int divisor  =  3;
+  printf("dividend mod divisor = %d\n", dividend % divisor);  
+  return 0;
+}
---
+++ code/operators/pre_decrement.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main () {
+  int variable = 1;
+  printf("variable is now: %d\n", --variable);
+  printf("variable is now: %d\n", --variable);
+  printf("variable at the end is: %d\n", variable);
+  return 0;
+}
