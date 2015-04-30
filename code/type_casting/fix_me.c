4b0dba8bc680e6289e2b98629670a146b7dc000b
Zhao Li <zhaol@hawaii.edu>
2015-04-30 03:12:22 +0000

    update lecture 12 type casting

--- code/type_casting/fix_me.c
+++ code/type_casting/fix_me.c
@@ -5,13 +5,13 @@ int main() {
   int integer2 = 4;
   float decimal = 4;
   
-  // both statements should print out correct answer to 3/4 (e.g. 0.75)
+  // both statements should print out the correct answer to 3/4 (e.g. 0.75)
   printf("int / float: %d\n", integer1/decimal);
   printf("int / int: %d\n", integer1/integer2);
   
   // you need to figure out what is the mod operator and how to use the mod operator
   // the output should be 1
-  printf("using the %% operator: %f\n", decimal % integer1);
+  printf("using the % operator: %f\n", decimal % integer1);
   
   return 0;
 }
