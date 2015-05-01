c78d49ad928172923db43612a56bf71426ee0884
Zhao Li <zhaol@hawaii.edu>
2015-05-01 15:09:28 +0000

    update arrays lecture and assignment

--- lectures/arrays/19_2.c
+++ lectures/arrays/19_2.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
-#include "USERNAME_19_2_magic_numbers.h"    // substitute with your own USERNAME
-#include "USERNAME_19_2_helper_functions.h" // substitute with your own USERNAME
-#include "USERNAME_19_2_helper_functions.c" // substitute with your own USERNAME
+#include "USERNAME_19_2_macros.h"    // substitute with your own USERNAME
+#include "USERNAME_19_2_functions.h" // substitute with your own USERNAME
+#include "USERNAME_19_2_functions.c" // substitute with your own USERNAME
 
 int main() {
   float list_of_values[MAX];
