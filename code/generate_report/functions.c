4ca0ca3e4b6a757e7b5a7ab9cb87828d5aed3a41
Zhao Li <zhaol@hawaii.edu>
2015-05-02 09:27:12 +0000

    update generate report lecture and code

--- code/generate_report/functions.c
+++ code/generate_report/functions.c
@@ -4,7 +4,7 @@ float net_balance(float account_balance, float minimum_balance_fee) {
 
 float get_account_balance() {
   float account_balance;  
-  printf ("Please enter the account balance: ");
+  printf ("Please enter the account balance:\n");
   scanf ("%f", &account_balance);
   return(account_balance);
 }
@@ -36,7 +36,7 @@ int is_account_below_minimum_balance(float account_balance, float minimum_balanc
   return(minimum_balance_fee_charged_flag);
 }
 
-int ask_to_exit() {
+char ask_to_exit() {
   char exit_program_flag;
   printf ("\n");
   printf ("Are you finished checking all your accounts?:\n");
@@ -52,7 +52,7 @@ void update_account_summary_information(int *total_number_of_accounts, float *to
 
 void output_account_summary_to_file(int total_number_of_accounts, float total_amount_in_accounts, float total_minimum_balance_fees, int total_accounts_with_minimum_balance_fees) {
   FILE *file_pointer;  
-  file_pointer = fopen("account_summary.txt", "w");
+  file_pointer = fopen("account_summary.output", "w");
   
   if (file_pointer == NULL) {
     printf("Error opening file\n");
@@ -72,7 +72,7 @@ void output_account_summary_to_file(int total_number_of_accounts, float total_am
 void output_list_of_account_balances(float account_balances[], int number_of_accounts) {
   int balances_display_counter = 1;
   FILE *file_pointer;  
-  file_pointer = fopen("list_of_account_balances.txt", "w");
+  file_pointer = fopen("list_of_account_balances.output", "w");
   
   if (file_pointer == NULL) {
     printf("Error opening file\n");
