d27b5ab1f45aa638f0c1660007f179b16d35b5d6
Zhao Li <zhaol@hawaii.edu>
2015-05-29 08:36:47 +0000

    add template for first assignment on variable and data type

---
+++ lectures/variables_data_types/03_1.template.c
@@ -0,0 +1,13 @@
+main() {
+  // declare the variable, i.e. int, float, double, char, etc.
+  // initialize the variable
+
+  /* user the variable:
+  printf("zero padding in back of a number: [fill this part in]);
+  printf("zero padding in front of a number: [fill this part in]);
+  printf("exponentials (base 10): [fill this part in]);
+  printf("truncation vs rounding of numbers: [fill this part in]);
+  printf("zero padding in back of a number: [fill this part in]);
+  printf("negative numbers: [fill this part in]);
+  */
+}
