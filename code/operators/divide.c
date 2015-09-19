f53f3e104e6d9cf303b9ba82cf199ee9bb3db898
Zhao Li <zhaol@hawaii.edu>
2015-09-19 09:37:32 +0000

    add unexpected 0 when dividing example

--- code/operators/divide.c
+++ code/operators/divide.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main () {
-  int dividend = 1;
-  int divisor  = 2;
+  int dividend = 2;
+  int divisor  = 4;
   printf("dividend / divisor = %d\n", dividend / divisor);  
 }
+
+// int 1 / int 2 / float 4
