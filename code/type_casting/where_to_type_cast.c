17d8feec582eb0aa85d06f8809aa9fbabd871221
Zhao Li <zhaol@hawaii.edu>
2015-09-24 02:00:24 +0000

    update type casting examples

--- code/type_casting/where_to_type_cast.c
+++ code/type_casting/where_to_type_cast.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
 int main() {
-  int i_am_an_integer = 5;
-  float i_am_a_float = 3;
+  int x;
+  int y;
+  
+  printf("x = ?\n");
+  scanf(" %d", &x);
+  printf("y = ?\n");
+  scanf(" %d", &y);
 
-  printf("(-x+(x^2-4y))/2y: %f\n", ((-i_am_an_integer)+(i_am_an_integer*i_am_an_integer)-(4*i_am_a_float))/(2*i_am_a_float));
+  printf("(-x+(x^2-4y))/2y= %d\n", ((-x)+(x*x)-(4*y))/(2*y));
   return 0;
 }
+
+// type casting, conversion specifiers, or variable declarations
