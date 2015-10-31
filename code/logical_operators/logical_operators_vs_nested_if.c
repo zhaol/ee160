6cfa53a4ccd02da11ce702cb8579f89b0fd18d64
Zhao Li <zhaol@hawaii.edu>
2015-10-31 07:59:04 +0000

    switch to 4 space indentation for easier lecture viewing and fix printf errors with extra arguments; also update in-class exercise for 25 logical operators, else if, and switch

--- code/logical_operators/logical_operators_vs_nested_if.c
+++ code/logical_operators/logical_operators_vs_nested_if.c
@@ -7,23 +7,24 @@
 
 // This program compares using logical operators with nested if statements
 int main() {
-  int number = 50;
-  
-  if (number >= 0) {
-    if (number <= 100) {
-      printf("This number is between 0 and 100 (using nested ifs)\n", number);
+    int number = 50;
+    
+    if (number >= 0) {
+        if (number <= 100) {
+            printf("This number is between 0 and 100 (using nested ifs)\n");
+        } else {
+            printf("This number is greater than 100\n");
+        }
     } else {
-      printf("This number is greater than 100\n");
+        printf("This number is less than 0\n");
     }
-  } else {
-    printf("This number is less than 0\n");
-  }
-  
-  if ((0 <= number) && (number <= 100)) {
-    printf("This number is between 0 and 100 (using logical operators)\n", number);
-  } else {
-    printf("This number could be less than 0 or greater than 100\n");
-  }
-
-  return 0;  
+    
+    printf("\n");
+    
+    if ((0 <= number) && (number <= 100)) {
+        printf("This number is between 0 and 100 (using logical operators)\n");
+    } else {
+        printf("This number could be less than 0 or greater than 100\n");
+    }
+    return 0;  
 }
