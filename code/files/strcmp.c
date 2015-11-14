3cafc9adfd1af04e882f5f11dc44235539d23765
Zhao Li <zhaol@hawaii.edu>
2015-11-14 17:38:22 +0000

    add more wording to strcmp program

--- code/files/strcmp.c
+++ code/files/strcmp.c
@@ -6,6 +6,6 @@ main() {
   char str2a[] = "does not match";
   char str2b[] = "doesn't match";
   
-  printf("%d\n", strcmp(str1a, str1b));
-  printf("%d\n", strcmp(str2a, str2b));
+  printf("when there's an exact match: %d\n", strcmp(str1a, str1b));
+  printf("when there's no exact match: %d\n", strcmp(str2a, str2b));
 }
