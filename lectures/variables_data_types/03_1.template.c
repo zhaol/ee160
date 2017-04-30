ca861cef7dae371a0b8ede798810e1ff9646300d
Zhao Li <zhaol@hawaii.edu>
2017-04-30 08:33:28 +0000

    fix typo

--- lectures/variables_data_types/03_1.template.c
+++ lectures/variables_data_types/03_1.template.c
@@ -2,7 +2,7 @@ main() {
   // declare the variable, i.e. int, float, double, char, etc.
   // initialize the variable
 
-  /* user the variable:
+  /* use the variable:
   printf("zero padding in back of a number: [fill this part in]);
   printf("zero padding in front of a number: [fill this part in]);
   printf("exponentials (base 10): [fill this part in]);
